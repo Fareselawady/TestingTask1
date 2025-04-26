b827ab758e10bf2bcb1ae492cda52ba73d1a81eb
Fareselawady <121154283+Fareselawady@users.noreply.github.com>
2025-04-26 16:14:46 +0300

    Add project files.

---
+++ DemoProjecttest/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using TestingTask1;
+namespace DemoProjecttest
+{
+    [TestFixture]
+    public class Class1
+    {
+        List<Employee> li;
+
+        [Test]
+        public void Testlogin()
+        {
+            Program p = new Program();
+            string x, y, z;
+            x = p.Login("", "");
+            y = p.Login("Admin", "Admin");
+            z = p.Login("asdasd", "23");
+
+            Assert.That(x, Is.EqualTo("Username or password are empty"));
+            Assert.That(y, Is.EqualTo("Login successful , Welcome Admin"));
+            Assert.That(z, Is.EqualTo("Login failed"));
+        }
+        [Test]
+        public void CheckDetails()
+        {
+            Program p = new Program();
+            li = p.AllUsers();
+            foreach (var item in li)
+            {
+                Assert.That(item.Id, Is.Not.Null);
+                Assert.That(item.Name, Is.Not.Null);
+                Assert.That(item.Salary, Is.Not.Null);
+                Assert.That(item.Gender, Is.Not.Null);
+            }
+        }
+        [Test]
+        public void GetuserDetails() 
+        {
+            Program p = new Program();
+            var x = p.GetEmployees(1);
+            foreach (var item in x)
+            {
+                Assert.That(item.Id, Is.EqualTo(1));
+                Assert.That(item.Name, Is.EqualTo("Mawlana"));
+                Assert.That(item.Gender, Is.EqualTo("Male"));
+                Assert.That(item.Salary, Is.EqualTo(1000));
+            }
+
+        }
+    }
+}
---
+++ TestingTask1/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace TestingTask1
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary{ get; set; }
+        public string Gender { get; set; }
+       
+        
+    }   
+    public class Program
+    {
+        public string Login(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return "Username or password are empty";
+            }
+            else if(username == "Admin" && password == "Admin")
+            {
+                return "Login successful , Welcome Admin";
+            }
+            else
+            {
+                return "Login failed";
+            }
+
+        }
+        public List<Employee> AllUsers() 
+        {
+            List<Employee> li = new List<Employee>();
+            li.Add(new Employee
+             {
+              Id  = 1  ,
+                Name = "Mawlana",
+                Gender = "Male" ,
+                Salary = 1000
+            });
+            li.Add(new Employee
+            {
+                Id = 2,
+                Name = "Gedo",
+                Gender = "Male",
+                Salary = 1000
+            });
+            li.Add(new Employee
+            {
+                Id = 3,
+                Name = "Nagah",
+                Gender = "Male",
+                Salary = 1000
+            });
+            li.Add(new Employee
+            {
+                Id = 4,
+                Name = "Matteo",
+                Gender = "Male",
+                Salary = 1000
+            });
+            return li;
+        }
+
+        public List<Employee> GetEmployees(int id) 
+        {
+            List<Employee> li2 = new List<Employee>();
+            Program p = new Program();
+            var li = p.AllUsers();
+            foreach (var item in li)
+            {
+                if (item.Id == id)
+                {
+                    li2.Add(item);
+                }
+              
+            } 
+            return li2;
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
